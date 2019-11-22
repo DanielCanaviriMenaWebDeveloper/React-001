42f4fb14759d181dca9c9c3c01181a6496620825
daniel <menacanaviridaniel@gmail.com>
2019-11-21 23:39:31 -0400

    Variables con var

---
+++ 01. JavaScript/1.1_ Variables-con-var/js/app.js
@@ -0,0 +1,12 @@
+//Declarar variables
+
+var aprendiendoJS =  true; //Variable con un valor boolean
+var variable2; //Iniciando variable sin valor
+
+aprendiendoJS = false; //Reescribiendo la variable 
+aprendiendoJS = 99; //Variable con un valor entero
+aprendiendoJS = "Daniel Alberto"; //Variable con un valor cadena de texto
+variable2 = " Web Developer"; //Asignando valor a la variable iniciada anteriormente
+console.log( aprendiendoJS );
+console.log( aprendiendoJS + variable2 );
+
