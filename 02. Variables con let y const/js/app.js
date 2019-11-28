9757f81e3f87e64e194e618dd86db1c4bdc5d281
daniel <menacanaviridaniel@gmail.com>
2019-11-27 23:41:34 -0400

    Variables con let y const

---
+++ 02. Variables con let y const/js/app.js
@@ -0,0 +1,13 @@
+//Variables con var
+/* var aprendiendo = "JavaScript"; */
+
+//Variables con const
+/* const aprendiendo = "JavaScript";  *//* Una variable constante siempre tiene que se inicializado */
+/* aprendiendo = true; */ //Una variable constante no permite redefinir su valor inicial, se usa en casos donde se desea seleccionar elementos de la interfaz del sitio web
+
+//Variables con let, tiene el mismo comportamiento que var
+let aprendiendo; /* Puede declarar una variable sin inicializar */
+aprendiendo = "Hola daniel"; /* Puede asignar el valor posteriormente */
+aprendiendo = "Como estas"; /* Puede redefinir el valor de la variable */
+
+console.log(aprendiendo);
